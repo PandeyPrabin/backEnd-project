0480bb6f0b88ee177842f0612f3b5669189993ac
Prabin Pandey <stormrider6225@gmail.com>
2020-03-26 18:05:36 +0200

    main file path changed to index.js from gulp and layout has been made

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require ('express')
 const app = new express()
-app.use(express.static('public'))
+var expressLayouts = require('express-ejs-layouts');
+const path = require ('path')
+var expressValidator = require('express-validator');
+var bodyParser = require('body-parser');
+
+
+//const router = express.router()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json({
+    type: 'application/*+json'
+}));
+
+//app.use(expressValidator());
+app.use(express.static(path.join(__dirname + '/public')));
+app.set('views', path.join(__dirname, 'views'));
+//app.engine('ejs', expejs({extname: 'ejs', defaultlayout: 'mainlayout', layoutsDir: __dirname+'/views/layouts'}))
+app.set('view engine', 'ejs');
+//app.use(expressLayouts);
+
+app.get('/', function(req, res){
+    res.render('admin/hello')
+})
+
+//app.use(express.static('public'))
+
 
 app.listen(4000, () =>{
     console.log('app listening on port http://127.0.0.1:4000/')
