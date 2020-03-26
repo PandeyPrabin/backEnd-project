4f6b005323fdafddb26e0bb09d5afdb8568eac3b
meAjit <38285723+meAjit@users.noreply.github.com>
2020-03-26 20:35:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const express = require ('express')
-const path = require ('path')
-const expressLayouts = require('express-ejs-layouts')
+const express = require ('express');
+const path = require ('path');
+const expressLayouts = require('express-ejs-layouts');
 
 
-const app = new express()
-const ejs = require ('ejs')
-app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public')); //this helps read all css and bootstrap files 
-                                                //from public folderfiles from 
-app.use(expressLayouts)
+const app = new express();
+const ejs = require ('ejs');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public')); //this helps direcct all css and bootstrap files 
+                                                //from public folder
+app.use(expressLayouts);
 
-app.get('/about' , (req, res) => {res.render('about')});
-app.get('/contact' , (req, res) => {res.render('contact')});
-app.get('/post' , (req, res) => {res.render('post')});
 
-var about = require('./routes/about')
-var contact = require('./routes/contact')
-var index = require('./routes/index')
-var post = require('./routes/post')
+var about = require('./routes/about');
+var contact = require('./routes/contact');
+var index = require('./routes/index');
+var post = require('./routes/post');
 
 
 
@@ -30,4 +27,4 @@ app.use('/post', post);
 
 app.listen(4000, () =>{
     console.log('app listening on port http://127.0.0.1:4000/')
-})
+});
