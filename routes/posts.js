cf36d8ee5070a2c7859a01b62610603e8668f7a8
Prabin Pandey <stormrider6225@gmail.com>
2020-03-27 15:23:51 +0200

    get request working

--- routes/posts.js
+++ routes/posts.js
@@ -2,18 +2,32 @@ var express = require ('express')
 var app = express()
 var Post = require('../models/post')
 
-app.get('/' , (req, res) => {
-    console.log('hello')
+// app.get('/' , (req, res) => {
+//     console.log('hello')
 
-var post = Post.find(function (err, result) {
-    if (err) throw err;
-    res.render('posts', {
-        data: result
-    });
-});
-//console.log(post)
+// var post = Post.find(function (err, result) {
+//     if (err) throw err;
+//     res.render('posts', {
+//         data: result
+//     });
+// });
+// //console.log(post)
 
-    res.redirect('/')
+//     res.redirect('/')
+// });
+
+app.get('/', function (req, res) {
+        console.log('hello')
+
+    Post.find({}, function(err, details){
+       
+        if(err){
+            console.log('Cannot fetch data from database!!!');
+        }else{
+            details.forEach(Post);
+            res.render('posts',{items: details});
+        }
+    })
 });
 
 module.exports = app;
