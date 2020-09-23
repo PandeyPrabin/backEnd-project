080a57230d3516fdb6e600580e137ffcc2214353
Prabin Pandey <stormrider6225@gmail.com>
2020-09-24 00:57:46 +0300

    new post can be created successfully on database

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var postSchema = new Schema({
-     name: {
+     title: {
         type: String, 
         required: true
         },
--- routes/post.js
+++ routes/post.js
@@ -5,22 +5,36 @@ var Post = require('../models/post')
 
 
  app.get('/' , (req, res) => {
-    res.render('post')
-});
-
-app.get('/new' , (req, res) => {
     res.render('newpost')
 });
 
+// app.get('/new' , (req, res) => {
+//     res.render('newpost')
+// });
+
 app.post('/post' , (req, res) => {
-    console.log(req.body)
+    //console.log(req.body)
 const post = new Post({
     title: req.body.title,
-    email: req.body.email
+    email: req.body.email,
+    phonenumber: req.body.phonenumber,
+    message: req.body.message
 
 })
 console.log(post)
-res.redirect('/')
+post.save();
+res.redirect('/');
+//res.redirect('/');
+// post.save(function (err, post) {
+//     if (err)
+// req.flash('success_msg','New post is created')
+// res.redirect('/')// Go back to the project site
+// });
+//  post.save()
+//     .then(post => {
+//     //req.flash('success_msg','New post is created')
+//     res.redirect('/')
+// })
 });
 
 module.exports = app;
