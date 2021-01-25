bf9c3a72d3c45cf63658978d3eadda2430b12a6e
meAjit <38285723+meAjit@users.noreply.github.com>
2021-01-25 22:07:57 +0200

    post render
    first post is rendered without id

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,20 @@
-var express = require ('express')
-var app = express()
+var express = require ('express');
+var app = express();
+var Posts = require('../models/posts');
 
 
-app.get('/' , (req, res) => {
-    res.render('post')
+app.get('/', function (req, res) {
+
+    Posts.find({},function(err, details){
+       
+        if(err){
+            console.log('Cannot fetch data from database!!!');
+        }else{
+            res.render('post',{
+                data: details,
+            });
+        };
+    });
 });
 
 module.exports = app;
